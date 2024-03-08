be3291393e1bd9f316e5e684cedc00ac4c644318
Pavel-VV <vahonin7@mail.ru>
2024-03-08 21:57:05 +0300

    fetch 12 movies

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,24 +1,35 @@
 import axios from "../../plugins/axios";
+import IDs from "../mock/imdb_top250";
 
 const moviesStore = {
   namespaced: true,
-  state: {},
-  getters: {},
+  state: {
+    top250IDs: IDs,
+    currentPage: 1,
+    moviesPerPage: 12,
+  },
+  getters: {
+    slicedIDs:
+      ({ top250IDs }) =>
+      (from, to) =>
+        top250IDs.slice(from, to),
+    currentPage: ({ currentPage }) => currentPage,
+    moviesPerPage: ({ moviesPerPage }) => moviesPerPage,
+  },
   mutations: {},
   actions: {
-    async fetchMovies({ commit }) {
-      console.log(commit);
+    async fetchMovies({ getters }) {
+      const { currentPage, moviesPerPage, slicedIDs } = getters;
+      const from = currentPage * moviesPerPage - moviesPerPage;
+      const to = currentPage * moviesPerPage;
       // tt0111161
-      const response = await axios.get("/", {
-        params: {
-          apikey: process.env.VUE_APP_API_KEY,
-          plot: "full",
-          i: "tt0111161",
-        },
-      });
+      const moviesToFetch = slicedIDs(from, to);
+      const requests = moviesToFetch.map((id) => axios.get(`/?i=${id}`));
+      const response = await Promise.all(requests);
       console.log(response);
     },
   },
 };
 
 export default moviesStore;
+// 22.50
