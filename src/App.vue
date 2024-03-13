42c4b575edd3b32cfab7871c6ed924eeac651434
Pavel-VV <vahonin7@mail.ru>
2024-03-13 22:07:06 +0300

    background-image create

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
   <div id="app">
-    <MoviesList :list="moviesList" />
+    <PosterBg :poster="poster" />
+    <MoviesList :list="moviesList" @changePoster="onChangePoster" />
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 import MoviesList from "./components/MoviesList";
+import PosterBg from "./components/PosterBg";
 
 export default {
   name: "App",
   components: {
     MoviesList,
+    PosterBg,
   },
+  data: () => ({
+    poster: "",
+  }),
   mounted() {},
   computed: {
     ...mapGetters("movies", ["moviesList"]),
   },
   methods: {
     ...mapActions("movies", ["fetchMovies"]),
+    onChangePoster(poster) {
+      this.poster = poster;
+    },
   },
 };
 </script>
 
 <style>
 #app {
+  position: relative;
   font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/PosterBg.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="background-poster"></div>
+</template>
+
+<script>
+export default {
+  name: "PosterBg",
+  props: {
+    poster: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    posterStyle() {
+      return "background-image";
+    },
+    posterBg() {
+      return this.poster
+        ? `url(${this.poster})`
+        : "linear-gradient(45deg, black 0%, rgb(87, 4, 87) 100%)";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.background-poster {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-size: cover;
+  z-index: -1;
+  /* background-image: linear-gradient(45deg, black 0%, rgb(87, 4, 87) 100%); */
+  background-position: center;
+  transition: all 0.2s ease;
+}
+</style>
