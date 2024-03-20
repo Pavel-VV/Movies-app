e22ff6e73160a2f7e7759f00422ccc9dc18e772b
Pavel-VV <vahonin7@mail.ru>
2024-03-20 22:16:53 +0300

    finish loader

---
+++ src/components/LoaderState.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="loader-wrap" v-if="getLoader">
+    <BSpinner type="grow" variant="light" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "LoaderState",
+  computed: {
+    ...mapGetters(["getLoader"]),
+  },
+};
+</script>
+
+<style scoped>
+.loader-wrap {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 999;
+}
+</style>
